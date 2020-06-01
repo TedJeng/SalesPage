6297a6da79cc32e59d914dc0037841997ed3086e
TedJeng <gn02087949@gmail.com>
2020-06-01 16:02:39 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@
 # 當發生錯誤時終止腳本運行
 set -e
 # 打包
-npm run build
+# npm run build
+npm run generate:gh-pages
 # 移動至到打包後的dist目錄 
 cd dist
 git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,42 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
+
 export default {
+  ...routerBase,
   mode: 'universal',
   /*
    ** Headers of the page
    */
   head: {
     title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: {
+    color: '#fff'
+  },
   /*
    ** Global CSS
    */
@@ -52,5 +69,8 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  router: {
+    base: '/shop/'
   }
 }
